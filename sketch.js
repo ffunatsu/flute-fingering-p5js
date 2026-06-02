7399bb03d46c980b248d5647ae966b296317bade
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-06-03 08:22:15 +0900

    update

--- sketch.js
+++ sketch.js
@@ -1,16 +1,21 @@
 var svg;
+// var path;
 
 function setup() {
   createCanvas(800, 800, SVG);
   svg = loadSVG('flute_finger.svg');
-  background(255);
-  fill(150);
-  stroke(150);
+
+//   background(255);
+//   fill(150);
+//   stroke(150);
 }
 
 function draw() {
 //   var r = frameCount % 200 * Math.sqrt(2);
   background(255);
   image(svg, 0, 0, 500, 500);
+
+  let path = querySVG('#a > path')[0];
+  path.attribute("fill", "rgb(255,255,0)");
 //   ellipse(0, 0, r, r);
 }
