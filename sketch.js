75b1d0d3c73ddaf0802b070235275f871086759e
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2026-06-03 09:43:09 +0900

    update: fingering

--- sketch.js
+++ sketch.js
@@ -13,58 +13,79 @@ function setup() {
 //   stroke(150);
 }
 
-var is_init = false
-var prev_path;
-var prev_color;
-
 function preload(){
     container = document.createElement('div')
 }
 
 var omps = [
-    "C",
-    "^C",
-    "D",
-    "^D",
-    "E",
-    "F",
-    "^F",
-    "G",
-    "^G",
-    "A",
-    "^A",
-    "B",
-    "c",
-    "^c",
-    "d",
-    "^d",
-    "e",
-    "f",
-    "^f",
-    "g",
-    "^g",
-    "a",
-    "^a",
-    "b",
-    "c'",
-    "^c'",
-    "d'",
-    "^d'",
-    "e'",
-    "f'",
-    "^f'",
-    "g'",
-    "^g'",
-    "a'",
-    "^a'",
-    "b'",
-    "c''",
-    "",
-    "",
+    "C",    "acdhijmnr",
+    "^C",   "acdhijnr",
+    "D",    "",
+    "^D",   "",
+    "E",    "",
+    "F",    "",
+    "^F",   "",
+    "G",    "",
+    "^G",   "",
+    "A",    "",
+    "^A",   "",
+    "B",    "",
+    "c",    "",
+    "^c",   "",
+    "d",    "",
+    "^d",   "",
+    "e",    "",
+    "f",    "",
+    "^f",   "",
+    "g",    "",
+    "^g",   "",
+    "a",    "",
+    "^a",   "",
+    "b",    "",
+    "c'",   "",
+    "^c'",  "",
+    "d'",   "",
+    "^d'",  "",
+    "e'",   "",
+    "f'",   "",
+    "^f'",  "",
+    "g'",   "",
+    "^g'",  "",
+    "a'",   "",
+    "^a'",  "",
+    "b'",   "",
+    "c''",  ""
 ];
 
+function has_music_svg(h){
+    if(h > 37){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function get_fingers(h){
+    if(h > 37){
+        return [];
+    }else{
+        return split(omps[h*2 + 1], "");
+    }
+}
+
+function get_omp(h){
+    if(h > 37){
+        return "";
+    }else{
+        return omps[h*2];
+    }
+}
+
 function music_svg(h){
-    ABCJS.renderAbc(container, "X:\n"+omps[h]+"2\n");
+    if(h > 37){
+        return;
+    }
+    ABCJS.renderAbc(container, "X:\n"+get_omp(h)+"2\n");
     return container.querySelector('svg');
 }
 
@@ -75,30 +96,40 @@ function draw() {
     var sec = millis() / 1000;
 //   console.log(sec)
 
-  var i = Math.floor(sec * 5) % 21;
+  var ind = Math.floor(sec * 5) % 21;
   var j = Math.floor(sec * 5) % 37;
 
-  image(music_svg(j), 0, 0, 200, 200);
+  if(has_music_svg(j)){
+    image(music_svg(j), 0, 0, 200, 200);
+  }
 
   image(svg, 0, 0, 500, 500);
 
-  var a = alphabets[i];
+  let white = "rgb(255,255,255)";
+  for(let i=0; i<21; ++i){
+    var a = alphabets[i];
+    let paths = querySVG('#' + a + ' > path');
+    if(paths.length > 0){
+        let path = paths[0];
+        path.style().fill = white;
+    }
 
-  if(!is_init){
-    is_init = true
   }
-//   var r = frameCount % 200 * Math.sqrt(2);
 
-  if(prev_path){
-    prev_path.style().fill = prev_color;
-  }
-  
-  let paths = querySVG('#' + a + ' > path');
-  if(paths.length > 0){
-    let path = paths[0];
-    prev_path = path;
-    prev_color = path.style().fill;
-    path.style().fill = "rgb(255,255,0)";
+  let fingers = get_fingers(ind);
+  if(fingers.length > 0){
+
+    for(let i=0; i<fingers.length; ++i){
+        // var a = alphabets[fingers[i]];
+        var a = fingers[i];
+        let paths = querySVG('#' + a + ' > path');
+        if(paths.length > 0){
+            let path = paths[0];
+            prev_path = path;
+            prev_color = path.style().fill;
+            path.style().fill = "rgb(255,0,0)";
+        }
+    }
   }
 
 //   ellipse(0, 0, r, r);
